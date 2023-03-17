30a4d0e7a82317c4e8c983210bca7b6b23881c26
Shumi Akter <shumiakter@Shumis-Air.fios-router.home>
2023-03-17 18:49:40 -0400

    Dynamic Xpath

---
+++ src/main/java/pages/DynamicXPath.java
@@ -0,0 +1,28 @@
+package pages;
+
+/*
+
+	https://m.shein.com/us/
+	tag with single attribute
+	//a[@id='header_logo_icon']
+	
+	https://www.uscis.gov/
+	Tag with multiple attribute 'or' logic
+	//input[@id = 'gsc-i-id1' and @name = 'search']
+	
+	https://www.uscis.gov/
+	tag with inner text
+	//a[text()='Return to top']")
+	
+	 https://www.facebook.com/
+	 Tag with inner text [means complete text]
+	//a[contains(text(), 'Create a Page')]
+	 
+	  https://www.facebook.com/
+	Tag with partial inner text [not complete inner text rather partial text as well as actual text] (most commonly used)
+	 //a[contains(text(), 'Fo')]
+	
+	*/
+	
+	
+
--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -14,7 +14,6 @@ public class HomePage {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);// PageFactory help to instaniate webelements
 	}
-
 	// 1st way: Most common way to write the webElement (@FindBy)
 	// Use of 'ClassName' attribute as locator
 	@FindBy(className = "fxg-user-options__sign-in-text")
@@ -30,21 +29,19 @@ public class HomePage {
 	@FindBy(how = How.XPATH, using = "//img[@class='fxg-header__logo']")
 	WebElement logo;
 
+	
 	public void clickLoginButton() throws InterruptedException {
 		loginButton.click();
 		Thread.sleep(4000);
 	}
-
 	public void clicktrackSearchButton() throws InterruptedException {
 		trackSearchButton.click();
 		Thread.sleep(5000);
 	}
-
 	public void clickTrackingID() throws InterruptedException {
 		trackingID.click();
 		Thread.sleep(4000);
 	}
-
 	public void clickLgo() throws InterruptedException {
 		logo.click();
 		Thread.sleep(2000);
